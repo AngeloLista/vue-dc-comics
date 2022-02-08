6d52fb4237883f717e19add3ac76935f52ca0a36
AngeloLista <abstractplayer@gmail.com>
2022-02-08 15:37:41 +0100

    added header structure

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <div class="container d-flex-j-between">
+            <figure>
+                <img src="../assets/dc-logo.png" alt="DC logo">
+            </figure>
+
+            <ul>
+                <li><a href="#">PROVA</a></li>
+                <li><a href="#">PROVA</a></li>
+                <li><a href="#">PROVA</a></li>
+                <li><a href="#">PROVA</a></li>
+                <li><a href="#">PROVA</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style>
+    figure {
+        display: inline-block;
+    }
+
+    .d-flex-j-between {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    ul,
+    ul li {
+        display: inline-block;
+    }
+</style>
