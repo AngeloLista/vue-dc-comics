6e580b5d26f3e943f267b7a4652829f4047fb241
AngeloLista <abstractplayer@gmail.com>
2022-02-08 17:16:41 +0100

    added main structure and content

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <div class="container">
+      <p>Content goes here</p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style lang="scss">
+@import "../assets/sass/_vars.scss";
+
+main {
+  background-color: $secondary_color;
+  height: 130px;
+  p {
+    color: white;
+    line-height: 130px;
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+}
+</style>
