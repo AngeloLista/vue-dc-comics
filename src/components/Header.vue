df2cc9e383eaa526e5714f8b46a74b1d98a8e77b
AngeloLista <abstractplayer@gmail.com>
2022-02-08 17:08:49 +0100

    added header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,21 @@
 <template>
-  <header>
-    <div class="container d-flex-j-between">
-      <figure>
-        <img src="../assets/img/dc-logo.png" alt="Logo DC" />
-      </figure>
+  <header class="container d-flex-j-between">
+    <img src="../assets/img/dc-logo.png" alt="Logo DC" />
 
-      <nav>
-        <ul>
-          <li><a href="#">PROVA</a></li>
-          <li><a href="#">PROVA</a></li>
-          <li><a href="#">PROVA</a></li>
-          <li><a href="#">PROVA</a></li>
-          <li><a href="#">PROVA</a></li>
-        </ul>
-      </nav>
-    </div>
+    <nav>
+      <ul>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -25,8 +26,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-figure {
-  display: inline-block;
+@import "../assets/sass/_vars.scss";
+
+header {
+  font-size: 0.8rem;
+  font-weight: 700;
+  height: 130px;
+  img {
+    width: 85px;
+  }
+  nav {
+    height: 100%;
+    ul {
+      height: 100%;
+      display: inline-block;
+      li {
+        display: inline-block;
+        line-height: 130px;
+        a {
+          line-height: 55px;
+          color: $secondary_color;
+          padding: 0 10px;
+          &:hover {
+            color: $main_color;
+            border-bottom: 5px solid $main_color;
+          }
+        }
+      }
+    }
+  }
 }
 
 .d-flex-j-between {
@@ -34,9 +62,4 @@ figure {
   justify-content: space-between;
   align-items: center;
 }
-
-ul,
-ul li {
-  display: inline-block;
-}
-</style>
+</style>s
