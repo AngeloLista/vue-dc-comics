4c9c306d8511037af5469717bdc856248a02fa70
AngeloLista <abstractplayer@gmail.com>
2022-02-10 18:13:12 +0100

    changed card component text size and added 'a' tag

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card">
-    <img class="card-thumbnail" :src="product.thumb" alt="" />
-    <span>{{ product.series }}</span>
+    <a href="#">
+      <img class="card-thumbnail" :src="product.thumb" alt="" />
+      <span>{{ product.series }}</span>
+    </a>
   </div>
 </template>
 
@@ -14,21 +16,25 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  color: white;
   display: inline-block;
   width: calc(100% / 6);
   margin: 30px 0;
-  .card-thumbnail {
-    height: 170px;
-    width: 170px;
-    margin-bottom: 15px;
+  a {
+    color: white;
 
-    object-fit: cover;
-    object-position: top;
-  }
-  span {
-    font-weight: 600;
-    text-transform: uppercase;
+    .card-thumbnail {
+      height: 170px;
+      width: 170px;
+      margin-bottom: 15px;
+
+      object-fit: cover;
+      object-position: top;
+    }
+    span {
+      font-weight: 600;
+      font-size: 0.9rem;
+      text-transform: uppercase;
+    }
   }
 }
 </style>
